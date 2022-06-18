d5ce269916ebb6a8722d4945d11866ddce408176
enrique1994mex <enrique.antonio1029@gmail.com>
2022-06-18 13:31:12 -0500

    Creacion del componente Layout

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,5 +1,12 @@
+import Login from "../Login";
 
 const Home = () => {
+
+    localStorage.setItem('user', 'Jose');
+
+    const user = localStorage.getItem('user'); 
+
+    
 
     return(
         <div>
--- src/views/Login/index.js
+++ src/views/Login/index.js
@@ -3,7 +3,7 @@ const Login = () => {
 
     return(
         <div>
-            Login 
+            Desde Login 
         </div>
     )
 } 

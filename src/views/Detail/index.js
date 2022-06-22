8790f30bbff275cd96ffd57de9022364122c024c
enrique1994mex <enrique.antonio1029@gmail.com>
2022-06-22 00:49:42 -0500

    Se construto el Carousel del componente Detail

--- src/views/Detail/index.js
+++ src/views/Detail/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import { useState, useEffect } from 'react';
 import { useParams } from "react-router-dom";
 import { pokemonDetail } from "../../api";
+import { Carousel, Container, Row, Col } from 'react-bootstrap';
+import ItemCarousel from "./ItemCarousel";
 
 const Detail = () => {
 
@@ -16,12 +18,34 @@ const Detail = () => {
             })
     }, [])
 
-    console.log(info)
+    if (!info) {
+        return <Container></Container>
+    }
 
-    return(
-        <div>
-            Detail
-        </div>
+    let valores = Object.values(info.sprites);
+
+    console.log(info.sprites)
+
+    return (
+        <Container>
+            <Row>
+                <Col>
+                    <Carousel fade variant="dark">
+                        {
+                            valores.filter(imagen => imagen !== null && typeof imagen !== 'object').map(img => (
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        src={img}
+                                        alt="First slide"
+                                    />
+                                </Carousel.Item>
+                            ))
+                        }
+                    </Carousel>
+                </Col>
+            </Row>
+        </Container>
     )
 }
 

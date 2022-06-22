2cb5c99db1898c34d7f8f7eb9dfde07ad777e8a2
enrique1994mex <enrique.antonio1029@gmail.com>
2022-06-21 22:37:12 -0500

    Se agrego la ruta del componente Detail

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,13 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../views/Home";
+import Detail from '../views/Detail';
 
 const RoutesComponents = () => (
     <BrowserRouter>
         <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="detail/:pokemon" element={<Detail/>}/>
         </Routes>
     </BrowserRouter>
 );
